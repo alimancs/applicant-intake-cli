9743e0ded8c919cd0b8f9b6724c08932fb485d2d
alimancs <aliman2952003@gmail.com>
2026-08-23 23:49:48 +0100

    update to menu

---
+++ src/main/java/com/applicant/intake/ui/ReviewerModeMenu.java
@@ -0,0 +1,36 @@
+package com.applicant.intake.ui;
+
+import com.applicant.intake.model.Menu;
+import com.applicant.intake.service.ApplicationService;
+
+public class ReviewerModeMenu extends Menu {
+
+    @Override
+    public void displayMenu() {
+        System.out.println("=========================================\n        APPLICANT INTAKE SYSTEM\n=========================================");
+        System.out.println("\n1. V\n2. Search Application\n3. Reviewer Mode\n4. List Applications\n5. Exit");
+    }
+
+    @Override
+    public void handleOption(int option) {
+        if (option == 1) {
+            ApplicationService.createApplication();
+            loadMenu();
+        } else if (option == 2) {
+            SearchApplicationMenu searchMenu = new SearchApplicationMenu();
+            searchMenu.loadMenu();
+        } else if (option == 3) {
+            System.out.println("You selected option 3: Reviewer Mode");
+            // Call the reviewer mode method or class here
+        } else if (option == 4) {
+            System.out.println("You selected option 4: List Applications");
+            // Call the list applications method or class here
+        } else if (option == 5) {
+            System.out.println("Exiting the system. Goodbye!");
+        } else {
+            System.out.println("Invalid option selected. Please try again.");
+            int newOption = getUserOption();
+            handleOption(newOption);
+        };
+    }
+}
